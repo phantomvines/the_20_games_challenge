aa68d827e223dca6e84dfc712db1d937f15dd0f0
phantomvines <lkrath@web.de>
2025-04-07 23:39:12 +0200

    added ball behaviour

---
+++ pong/scenes/main.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+# TODO: get signal from ball when hit area hit
+# TODO: labels for Score
---
+++ pong/scripts/ball.gd
@@ -0,0 +1,48 @@
+extends Area2D
+
+# ball speed and initial velocity
+@export var ball_speed = 100
+var ball_dir = Vector2(1,2).normalized()
+
+# Emmission length of particles
+var emission_length = 0.1
+
+signal ball_hit()
+
+func _process(delta: float) -> void:
+	# move ball in direction
+	position += ball_dir * ball_speed * delta
+
+func _on_body_entered(body: Node2D) -> void:
+	# Bounce off of Player
+	if body.is_in_group("players"):
+		ball_dir.x *= -1
+		$CPUParticles2D.emitting = true # emit particles
+		$Timer.wait_time = emission_length
+		$Timer.start()
+	
+	# Bounce off of Top and Bottom
+	if body.is_in_group("bounding_box"):
+		ball_dir.y *= -1
+		$CPUParticles2D.emitting = true # emit particles
+		$Timer.wait_time = emission_length
+		$Timer.start()
+		
+	# ball hits left hit area, teleported to middle
+	if body.is_in_group("left_hit_area"):
+		position = Vector2(200, 150)
+		ball_dir = Vector2(1,2).normalized()
+		ball_hit.emit("left")
+	
+	# ball hits right hit area, teleported to middle
+	if body.is_in_group("right_hit_area"):
+		position = Vector2(200, 150)
+		ball_dir = Vector2(1,2).normalized()
+		ball_hit.emit("right")
+
+func _ready() -> void:
+	$CPUParticles2D.emitting = false
+
+func _on_timer_timeout() -> void:
+	# stop emitting particles
+	$CPUParticles2D.emitting = false
