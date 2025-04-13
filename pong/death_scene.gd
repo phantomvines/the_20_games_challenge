7ab9fa0874f3c2557af9612b14a98a9ea9fa5d2c
phantomvines <lkrath@web.de>
2025-04-13 13:46:09 +0200

    death scene layout

---
+++ pong/death_scene.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+func _on_retry_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+	# close scene
+	get_tree().free()
--- pong/scenes/main.gd
+++ pong/scenes/main.gd
@@ -1,7 +1,5 @@
 extends Node2D
 
-# TODO: get signal from ball when hit area hit
-# TODO: labels for Score
 var lifes_player_1 = 3
 var lifes_player_2 = 3
 
@@ -12,4 +10,3 @@ func _on_ball_ball_hit(side: String) -> void:
 	else:
 		lifes_player_2 -= 1
 		$lives_player_2.text = "Lifes: " + str(lifes_player_2)
-	pass # Replace with function body.
