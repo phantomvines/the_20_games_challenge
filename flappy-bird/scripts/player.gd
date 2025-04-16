07df7ea44ae7a71537381b81b786f75e14d243a6
phantomvines <lkrath@web.de>
2025-04-16 11:39:46 +0200

    added floor, does not collide

--- flappy-bird/scripts/player.gd
+++ flappy-bird/scripts/player.gd
@@ -15,8 +15,20 @@ func _physics_process(delta: float) -> void:
 	# add gravity to direction every frame -> player falling
 	movement_dir_speed.y += delta*gravity
 	velocity = movement_dir_speed
+	
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		if collision.get_collider() is StaticBody2D:
+			print("Collided with static body!")
+	
 	move_and_slide()
 
 func flap() -> void:
 	# change direction to up and add velocity
 	movement_dir_speed.y = flap_force
+
+func _on_floor_area_entered(area: Area2D) -> void:
+	# TODO Death
+	print("floor hit")
+	position = Vector2(160, 340) # currently just teleport back to start
+	velocity = Vector2.ZERO
