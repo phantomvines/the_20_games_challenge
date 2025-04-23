63a97a92bcd27148f6a01ce01bb734aef6693339
phantomvines <lkrath@web.de>
2025-04-23 13:38:53 +0200

    confusion about area2d

--- flappy-bird/scenes/flames.gd
+++ flappy-bird/scenes/flames.gd
@@ -7,3 +7,8 @@ func _process(delta: float) -> void:
 	
 	if position.x < -60:
 		queue_free()
+
+
+func _on_point_area_area_entered(area: Area2D) -> void:
+	print("point")
+	pass # Replace with function body.
