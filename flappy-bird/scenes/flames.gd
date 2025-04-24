6b8e060343d995c2d974f0280fbbaf7db087425f
phantomvines <lkrath@web.de>
2025-04-24 10:47:21 +0200

    collision works

--- flappy-bird/scenes/flames.gd
+++ flappy-bird/scenes/flames.gd
@@ -8,12 +8,20 @@ func _process(delta: float) -> void:
 	if position.x < -60:
 		queue_free()
 
+#func _ready() -> void:
+#	$PointArea.body_entered.connect(_on_point_area_body_entered)
 
-func _on_point_area_area_entered(area: Area2D) -> void:
+
+#func _on_point_area_area_entered(area: Area2D) -> void:
+#	print("point")
+#	pass # Replace with function body.
+
+
+func _on_point_area_body_entered(_body: Node2D) -> void:
 	print("point")
 	pass # Replace with function body.
 
 
-func _on_point_area_body_entered(body: Node2D) -> void:
-	print("point")
+func _on_pipe_collision_body_entered(body: Node2D) -> void:
+	
 	pass # Replace with function body.
