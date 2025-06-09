9b5fea05b8faa14139212a96518b77fb4db53a27
phantomvines <lkrath@web.de>
2025-06-09 22:06:50 +0200

    simple screen wrapping works

--- asteroids/main.gd
+++ asteroids/main.gd
@@ -10,3 +10,19 @@ func _physics_process(delta: float) -> void:
 	# Deathd
 	if Scenemanager.lifes <= 0:
 		queue_free()
+
+# when asteroid or character enters, loop to the other side
+func _on_bounding_up_down_body_entered(body: Node2D) -> void:
+	print(body.position )
+	if body.position.y < 10: # upper area reached
+		body.position.y = 300
+	else: # lower area reached
+		body.position.y = 0 
+	
+
+# when asteroid or character enters, loop to the other side
+func _on_bounding_left_right_body_entered(body: Node2D) -> void:
+	if body.position.x < 10: # left area reached
+		body.position.x = 400
+	else: # right area reached
+		body.position.x = 0 
