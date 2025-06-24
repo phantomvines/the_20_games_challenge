aaf231e5646003d04b36d7de7b22b450f8bd3d67
phantomvines <lkrath@web.de>
2025-06-24 18:02:40 +0200

    init board

--- conways-game-of-life/main.gd
+++ conways-game-of-life/main.gd
@@ -1,3 +1,16 @@
 extends Node2D
 
-var 
+var board = []
+
+func _ready() -> void:
+	init_array(10)
+	print(board)
+
+
+
+# makes a quadratic 2D-Array filled with zeros, based on size
+func init_array(size) -> void:
+	for i in range(size):
+		board.append([])
+		for j in range(size): 
+			board[i].append(0)
