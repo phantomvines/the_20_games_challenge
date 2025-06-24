b556357aab603959ddbabc9a79e8870fe2f537a1
phantomvines <lkrath@web.de>
2025-06-24 18:08:17 +0200

    fill random

--- conways-game-of-life/main.gd
+++ conways-game-of-life/main.gd
@@ -4,9 +4,14 @@ var board = []
 
 func _ready() -> void:
 	init_array(10)
+	fill_random()
 	print(board)
 
-
+# fill the board with 0 and 1 randomly
+func fill_random() -> void:
+	for i in range(board.size()):
+		for j in range(board[0].size()):
+			board[i][j] = randi_range(0,1)
 
 # makes a quadratic 2D-Array filled with zeros, based on size
 func init_array(size) -> void:
