1401a1119864e6cff4983920a34f94fc5539a15a
phantomvines <lkrath@web.de>
2025-06-26 11:42:32 +0200

    started tileset, more logic

--- conways-game-of-life/main.gd
+++ conways-game-of-life/main.gd
@@ -3,12 +3,33 @@ extends Node2D
 var board = []
 
 func _ready() -> void:
-	init_array(10)
+	board = init_array(10)
 	fill_random()
 	print(board)
 
+# update the board once
 func update_conway() -> void:
-	var board_new = 
+	# new, blank board
+	var board_new = init_array(board.size)
+	for i in range(board.size()):
+		for j in range(board.size()):
+			# all of the middle squares, not the edges
+			if i >= 1 and j >= 1 and i <= board.size() and j <= board.size():
+				var total = 0
+				
+				# add all states of surrounding and self together
+				for x in range(i-1,i+1):
+					for y in range(j-1,j+1):
+						total += board[x][y]
+				
+				# subtract own value
+				total -= board[i][j]
+				
+				# set new live values
+				if total == 3 or total == 2 and board[i][j] == 1: # sustain
+					board_new[i][j] = 1
+				elif total == 4 and board[i][j] == 0: # reproduction
+					board_new[i][j] = 1
 
 # fill the board with 0 and 1 randomly
 func fill_random() -> void:
