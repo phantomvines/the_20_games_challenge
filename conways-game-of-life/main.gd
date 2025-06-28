6351e7f91c2cad684fef0b07c983ba3a810c0035
phantomvines <lkrath@web.de>
2025-06-28 16:10:20 +0200

    fixed logic a bit

--- conways-game-of-life/main.gd
+++ conways-game-of-life/main.gd
@@ -3,7 +3,7 @@ extends Node2D
 var board = []
 
 func _ready() -> void:
-	board = init_array(10)
+	board = init_array(100)
 	fill_random()
 	print(board)
 	draw_board()
@@ -12,12 +12,6 @@ func _ready() -> void:
 func draw_board() -> void:
 	for i in range(board.size()):
 		for j in range(board.size()):
-			# Get the tile at position
-			#var tile = $TileMapLayer.get_cellv(Vector2(i, j))
-			# Set a tile at position (5, 3) to tile ID 1
-			#$TileMapLayer.set_cellv(Vector2(i, j), 1)
-			# Force redraw if needed
-			#$TileMapLayer.update()
 			# get state of cell
 			var cell_alive = board[i][j] == 1
 			
@@ -28,7 +22,6 @@ func draw_board() -> void:
 			var atlas_coords = Vector2(0,0)
 			if cell_alive:
 				atlas_coords = Vector2(2,0)
-				print(1)
 			
 			# set cell in tilemaplayer
 			$TileMapLayer.set_cell(Vector2(i,j), tile_id, atlas_coords)
@@ -36,16 +29,16 @@ func draw_board() -> void:
 # update the board once
 func update_conway() -> void:
 	# new, blank board
-	var board_new = init_array(board.size)
+	var board_new = init_array(board.size())
 	for i in range(board.size()):
 		for j in range(board.size()):
 			# all of the middle squares, not the edges
-			if i >= 1 and j >= 1 and i <= board.size()-1 and j <= board.size()-1:
+			if i >= 1 and j >= 1 and i <= board.size()-2 and j <= board.size()-2:
 				var total = 0
 				
 				# add all states of surrounding and self together
-				for x in range(i-1,i+1):
-					for y in range(j-1,j+1):
+				for x in range(i-1,i+2):
+					for y in range(j-1,j+2):
 						total += board[x][y]
 				
 				# subtract own value
@@ -56,6 +49,7 @@ func update_conway() -> void:
 					board_new[i][j] = 1
 				elif total == 4 and board[i][j] == 0: # reproduction
 					board_new[i][j] = 1
+	board = board_new
 
 # fill the board with 0 and 1 randomly
 func fill_random() -> void:
@@ -64,10 +58,15 @@ func fill_random() -> void:
 			board[i][j] = randi_range(0,1)
 
 # makes a quadratic 2D-Array filled with zeros, based on size
-func init_array(size):
+func init_array(size_board):
 	var new_board = []
-	for i in range(size):
+	for i in range(size_board):
 		new_board.append([])
-		for j in range(size): 
+		for j in range(size_board): 
 			new_board[i].append(0)
 	return new_board
+
+
+func _on_update_timer_timeout() -> void:
+	update_conway()
+	draw_board()
